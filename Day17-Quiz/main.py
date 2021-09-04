74236bf27ea9a7234f38325f58f0baa75a120205
s-nc <82464950+s-nc@users.noreply.github.com>
2021-09-04 16:48:58 +0100

    Add files via upload
    # initial upload

--- Day17-Quiz/main.py
+++ Day17-Quiz/main.py
@@ -1,1 +1,26 @@
+from turtle import Turtle, Screen
 
+timmy = Turtle()
+
+"""
+
+Note: this is really day 18
+timmy.forward(150)
+timmy.left(90)
+timmy.forward(150)
+timmy.left(90)
+timmy.forward(150)
+timmy.left(90)
+timmy.forward(150)
+timmy.left(90)
+"""
+
+for i in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+
+screen = Screen()
+screen.exitonclick()
