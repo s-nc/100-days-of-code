44dcf7ebb8c3d406d5507e3725c28ac78aa60efa
s-nc <82464950+s-nc@users.noreply.github.com>
2021-09-16 22:42:31 +0100

    Add files via upload
    Initial commit

--- Day33-Kanye-Tweets/main.py
+++ Day33-Kanye-Tweets/main.py
@@ -1,1 +1,35 @@
+import requests
+from tkinter import *
+
+"""response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+print(data)"""
+
+
+def quote_kanye():
+    response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status()
+    quotation = response.json()["quote"]
+    canvas.itemconfig(quote_label, text=quotation)
+
+
+window = Tk()
+window.title("Kanye Motivation")
+window.config(padx=30, pady=30)
+
+canvas = Canvas(width=300, height=414)
+speech_bubble = PhotoImage(file="background.png")
+background = canvas.create_image(150, 207, image=speech_bubble)
+quote_label = canvas.create_text(150, 207, text="Welcome to my fountain of wisdom."
+                                 , width=250, font=("Arial", 20, "bold"), fill="white")
+canvas.pack()
+
+kanye = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye, command=quote_kanye)
+kanye_button.pack()
+
+
+window.mainloop()
 
