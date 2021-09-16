9ad20b958f1481b24049b64db7bc485635b899cc
s-nc <82464950+s-nc@users.noreply.github.com>
2021-09-16 22:51:29 +0100

    Add files via upload
    Initial commit

--- Day16-Coffee-Machine/main.py
+++ Day16-Coffee-Machine/main.py
@@ -1,1 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffe_maker2000 = CoffeeMaker()
+menu = Menu()
+
+on = True
+
+while on:
+    options = menu.get_items()
+    choice = input(f"What would you like to drink? Choose from {options} ")
+    if choice == "off":
+        on = False
+    elif choice == "report":
+        coffe_maker2000.report()
+        money_machine.report()
+    else:
+        order = menu.find_drink(choice)
+        if coffe_maker2000.is_resource_sufficient(order):
+            if money_machine.make_payment(order.cost):
+                coffe_maker2000.make_coffee(order)
