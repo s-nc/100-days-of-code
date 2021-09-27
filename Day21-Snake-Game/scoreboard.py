4f2eb855a38a565eeb5809791cc187a914d15b37
s-nc <82464950+s-nc@users.noreply.github.com>
2021-09-27 13:56:05 +0100

    Update scoreboard.py

--- Day21-Snake-Game/scoreboard.py
+++ Day21-Snake-Game/scoreboard.py
@@ -35,10 +35,3 @@ class Scoreboard(Turtle):
         self.score = 0
         self.update_scoreboard()
 
-    """def game_over(self):
-        self.goto((0,0))
-        self.color("white")
-        self.write("GAME OVER", align=score_format["ALIGNMENT"])"""
-
-
-
